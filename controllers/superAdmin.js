744ca093878e29c390a5a425a107b49443287010
Abhijith T R <2020csb1062@iitrpr.ac.in>
2022-06-23 20:19:34 +0530

    Register super user

--- controllers/superAdmin.js
+++ controllers/superAdmin.js
@@ -2,19 +2,6 @@ const { StatusCodes } = require("http-status-codes");
 const { UnauthenticatedError, BadRequestError } = require("../errors");
 const Admin = require("../models/admin");
 
-const registerSuper = async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    throw new BadRequestError("Please enter valid credentials");
-  }
-  const check = await Admin.findOne({ superAdmin: true });
-  if (check) {
-    throw new BadRequestError("One super admin already exists");
-  }
-  const admin = await Admin.create({ superAdmin: true, name, email, password });
-  res.status(StatusCodes.CREATED).json({ msg: "Super Admin Created" });
-};
-
 module.exports = {
   registerSuper,
 };
