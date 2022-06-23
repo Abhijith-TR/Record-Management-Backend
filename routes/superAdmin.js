8921744a7662171347296603d8563124bcc25bc5
Abhijith T R <2020csb1062@iitrpr.ac.in>
2022-06-23 20:23:44 +0530

    Major bug fix

--- routes/superAdmin.js
+++ routes/superAdmin.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { adminRegister } = require("../controllers/admin");
-const { registerSuper } = require("../controllers/superAdmin");
 
-router.route("/start").post(registerSuper);
 router.route("/register/admin").post(adminRegister);
 router.route("/delete/admin/:email").delete();
 
